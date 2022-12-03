d36d5e0c74d2f687e83f0edca0a86f5f0314ac7a
MohamedBouarada <bouarada7.mb@gmail.com>
2022-12-03 17:26:14 +0100

    adding video to landing page

--- src/app/client/client.module.ts
+++ src/app/client/client.module.ts
@@ -4,10 +4,12 @@ import { AppRoutingModule } from 'src/app/app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from '../material/material.module';
 import { HomeComponent } from './pages/home/home.component';
+import { VideoComponent } from './components/video/video.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
+    VideoComponent,
   ],
   exports:[],
   imports: [
