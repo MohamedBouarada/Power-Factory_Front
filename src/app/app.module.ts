cc4f6a55a37e75742a5d8c07819e5007b6d23419
MohamedBouarada <bouarada7.mb@gmail.com>
2022-12-13 22:18:08 +0100

    adding partners component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,21 +6,19 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 import { ClientModule } from './client/client.module';
-
-
+import { CarouselModule } from 'ngx-owl-carousel-o';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
     ClientModule,
+    CarouselModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/client/client.module.ts
+++ src/app/client/client.module.ts
@@ -9,6 +9,8 @@ import { DefaultVideoPipe } from './pipes/default-video.pipe';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { AboutUsComponent } from './components/about-us/about-us.component';
 import { BlogsComponent } from './components/blogs/blogs.component';
+import { PartnersComponent } from './components/partners/partners.component';
+import { CarouselModule } from 'ngx-owl-carousel-o';
 
 @NgModule({
   declarations: [
@@ -18,6 +20,7 @@ import { BlogsComponent } from './components/blogs/blogs.component';
     NavbarComponent,
     AboutUsComponent,
     BlogsComponent,
+    PartnersComponent,
   ],
   exports:[],
   imports: [
@@ -25,6 +28,7 @@ import { BlogsComponent } from './components/blogs/blogs.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
+    CarouselModule
   ]
 })
 export class ClientModule { }
