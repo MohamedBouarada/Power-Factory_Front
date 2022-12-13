dccbcb735b008e10289f1ff7b1d9a2e6a6ebde8c
MohamedBouarada <bouarada7.mb@gmail.com>
2022-12-13 23:12:55 +0100

    adding owlCarousel to blogs component

--- src/app/client/components/blogs/blogs.component.ts
+++ src/app/client/components/blogs/blogs.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-blogs',
@@ -7,6 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlogsComponent implements OnInit {
 
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: true,
+    dots: false,
+    navSpeed: 700,
+    navText: ['<mat-icon>arrow_left</mat-icon>', '<mat-icon >arrow_right</mat-icon>'],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 2
+      },
+      740: {
+        items: 3
+      },
+      940: {
+        items: 3
+      }
+    },
+    nav: true
+  }
   constructor() { }
 
   ngOnInit(): void {
