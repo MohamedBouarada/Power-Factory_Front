132245936a45f775e5b93d161eb8c4c98b4a7c8d
MohamedBouarada <bouarada7.mb@gmail.com>
2022-12-19 17:51:10 +0100

    making promo-products dynamic

--- src/app/client/components/promotional-products/promotional-products.component.ts
+++ src/app/client/components/promotional-products/promotional-products.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PromotionalProductsInterface } from './promotional-products-interface';
 
 @Component({
   selector: 'app-promotional-products',
@@ -6,14 +7,80 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./promotional-products.component.scss'],
 })
 export class PromotionalProductsComponent implements OnInit {
-  constructor() {}
+  listOfProducts: PromotionalProductsInterface[] = [
+    {
+      name: 'Incidid Tongue Bar',
+      discount: -9,
+      principaleImage: '/assets/images/post-3.jpg',
+      otherImages: [
+        '/assets/images/post-3.jpg',
+        '/assets/images/post-4.jpg',
+        '/assets/images/post-6.jpg',
+      ],
+      rating: 5,
+      oldPrice: 89,
+      newPrice: 81,
+    },
+    {
+      name: 'Incidid Tongue Bar',
+      discount: -9,
+      principaleImage: '/assets/images/post-3.jpg',
+      otherImages: [
+        '/assets/images/post-6.jpg',
+        '/assets/images/post-4.jpg',
+        '/assets/images/post-6.jpg',
+      ],
+      rating: 0,
+      oldPrice: 89,
+      newPrice: 81,
+    },
+    {
+      name: 'Incidid Tongue Bar',
+      discount: -9,
+      principaleImage: '/assets/images/post-3.jpg',
+      otherImages: [
+        '/assets/images/post-3.jpg',
+        '/assets/images/post-4.jpg',
+        '/assets/images/post-6.jpg',
+      ],
+      rating: 5,
+      oldPrice: 89,
+      newPrice: 81,
+    },
+    {
+      name: 'Incidid Tongue Bar',
+      discount: -9,
+      principaleImage: '/assets/images/post-6.jpg',
+      otherImages: [
+        '/assets/images/post-3.jpg',
+        '/assets/images/post-4.jpg',
+        '/assets/images/post-6.jpg',
+      ],
+      rating: 2,
+      oldPrice: 89,
+      newPrice: 81,
+    },
+  ];
 
-  ngOnInit(): void {}
-
-  url: string = '/assets/images/post-3.jpg';
-  imageChange(event: any) {
-    if(event.target.src){
-      this.url = event.target.src;
+  stars=[1,2,3,4,5];
+  constructor() {
+  }
+  images:string[]=[]
+  ngOnInit(): void {
+    //saving the pricipale images of each product
+    for(var product of this.listOfProducts){
+      this.images.push(product.principaleImage)
     }
+  }
+
+  
+  imageChange(event: any,product:PromotionalProductsInterface) {
+    
+    if (event.target.src) {
+      product.principaleImage = event.target.src;
+    }
+  }
+  getFirstImage(product:PromotionalProductsInterface){
+    product.principaleImage=this.images[this.listOfProducts.indexOf(product)];
   }
 }
