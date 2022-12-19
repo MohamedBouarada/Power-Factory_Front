6b362079b0465cac797e1433071ac8072f247fd4
MohamedBouarada <bouarada7.mb@gmail.com>
2022-12-19 18:30:15 +0100

    adding carousel to promo-products

--- src/app/client/components/promotional-products/promotional-products.component.ts
+++ src/app/client/components/promotional-products/promotional-products.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PromotionalProductsInterface } from './promotional-products-interface';
-
+import { OwlOptions } from 'ngx-owl-carousel-o';
 @Component({
   selector: 'app-promotional-products',
   templateUrl: './promotional-products.component.html',
@@ -60,12 +60,65 @@ export class PromotionalProductsComponent implements OnInit {
       oldPrice: 89,
       newPrice: 81,
     },
+    {
+      name: 'Incidid Tongue Bar',
+      discount: -9,
+      principaleImage: '/assets/images/post-6.jpg',
+      otherImages: [
+        '/assets/images/post-3.jpg',
+        '/assets/images/post-4.jpg',
+        '/assets/images/post-6.jpg',
+      ],
+      rating: 2,
+      oldPrice: 89,
+      newPrice: 81,
+    },
+    {
+      name: 'Incidid Tongue Bar',
+      discount: -9,
+      principaleImage: '/assets/images/post-6.jpg',
+      otherImages: [
+        '/assets/images/post-3.jpg',
+        '/assets/images/post-4.jpg',
+        '/assets/images/post-6.jpg',
+      ],
+      rating: 2,
+      oldPrice: 89,
+      newPrice: 81,
+    },
   ];
 
   stars=[1,2,3,4,5];
+
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: true,
+    dots: false,
+    navSpeed: 700,
+    navText: ['<i class="fa fa-caret-left"></i>', '<i class="fa fa-caret-right"></i>'],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 2
+      },
+      740: {
+        items: 3
+      },
+      940: {
+        items: 4
+      }
+    },
+    nav: true
+  }
+  
   constructor() {
   }
   images:string[]=[]
+  
   ngOnInit(): void {
     //saving the pricipale images of each product
     for(var product of this.listOfProducts){
