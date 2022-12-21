061b32429da9f5d2683defdea72d8b4378197914
MohamedBouarada <bouarada7.mb@gmail.com>
2022-12-21 23:22:35 +0100

    make the navbar fixed and change bg color on scroll

--- src/app/client/components/navbar/navbar.component.ts
+++ src/app/client/components/navbar/navbar.component.ts
@@ -1,25 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
-
-  navbarIcon:string="menu";
-  constructor() { }
-
-  ngOnInit(): void {
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    let element = document.querySelector('.navbar') as HTMLElement;
+    if (window.pageYOffset >= element.clientHeight) {
+      element.classList.add('navbar-inverse');
+    } else {
+      element.classList.remove('navbar-inverse');
+    }
   }
-  activateMenu(){
-    
-    const navLinks = document.querySelector(".navLinks")
-    navLinks!.classList.toggle('mobile-menu')
-    if(this.navbarIcon=="menu"){
-      this.navbarIcon="close"
-    }else{
-      this.navbarIcon="menu";
+  navbarIcon: string = 'menu';
+  constructor() {}
+
+  ngOnInit(): void {}
+  activateMenu() {
+    const navLinks = document.querySelector('.navLinks');
+    navLinks!.classList.toggle('mobile-menu');
+    if (this.navbarIcon == 'menu') {
+      this.navbarIcon = 'close';
+    } else {
+      this.navbarIcon = 'menu';
     }
   }
 }
